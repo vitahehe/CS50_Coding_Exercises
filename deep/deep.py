13393ec6c545b0ff3624bbc93d373d0c54edab7b
vitahehe <vita.stefanija@gmail.com>
2024-07-05 14:06:19 +0000

    Fri, Jul 5, 2024, 4:06 PM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?").strip().lower()
 
 if answer == "42" or "forty-two" or "forty two":
- print("yes")
+ print("Yes")
 else :
- print("no")
+ print("No")
 
