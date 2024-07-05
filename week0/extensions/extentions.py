53e5c24b120e88f213e38fa289bbdae3f48f4bbe
vitahehe <vita.stefanija@gmail.com>
2024-07-05 16:21:42 +0000

    Fri, Jul 5, 2024, 6:21 PM +02:00

--- week0/extensions/extentions.py
+++ week0/extensions/extentions.py
@@ -1,5 +1,17 @@
 file = input("whats the file name?").strip().lower()
 if ".pdf" in file:
-    print("image/pdf")
+    print("application/pdf")
+if ".gif" in file:
+    print("image/gif")
+if ".jpg" in file:
+    print("image/jpg")
+if ".jepg" in file:
+    print("image/jepg")
+if ".png" in file:
+    print(image/png)
+if ".txt" in file:
+    print("text/plain")
+if ".zip" in file:
+    print("application/zip")
 else:
-    print("fuckof")
+    print("application/octet-stream")
