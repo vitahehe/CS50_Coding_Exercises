d677910b081c14aae1b538f0e38b5a84d1b8bc71
vitahehe <vita.stefanija@gmail.com>
2024-07-05 19:25:57 +0000

    Fri, Jul 5, 2024, 9:25 PM +02:00

--- week0/extensions/extensions.py
+++ week0/extensions/extensions.py
@@ -8,7 +8,7 @@ elif ".jpg" in file:
 elif ".jpeg" in file:
     print("image/jpeg")
 elif ".png" in file:
-    print(image/png)
+    print("image/png")
 elif ".txt" in file:
     print("text/plain")
 elif ".zip" in file:
