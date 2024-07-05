6fb34c1384c24d360a2db9e38d964c588ab246e6
vitahehe <vita.stefanija@gmail.com>
2024-07-05 14:58:43 +0000

    Fri, Jul 5, 2024, 4:58 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,8 +1,8 @@
 greeting = input("Greeting good sir").strip().lower()
 response = list(greeting)
 if (response[0] == "h" and greeting != "hello"):
-    print("20$")
+    print("$20")
 elif greeting == "hello":
-    print("0$")
+    print("$0")
 else:
-    print("100$")
+    print("$100")
