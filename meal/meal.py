e480bf478f11f1dac7441e88b8cf0dc403f4b2de
vitahehe <vita.stefanija@gmail.com>
2024-07-06 18:02:25 +0000

    Sat, Jul 6, 2024, 8:02 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,21 @@
-def main():
 
-      time = str(input("what time is it?").lower().strip())
 
-                   def replacesymbol(x ,y , z):
-                  return x.replace(y,z)
+time = str(input("what time is it?").lower().strip())
+
+def replacesymbol(x ,y , z):
+    return x.replace(y,z)
 convertedtime = float(replacesymbol(time, ":", "."))
-     if convertedtime is in 
+breakfast1 = 07.00
+breakfast2 = 08.00
+lunch1 = 12.00
+lunch2 = 13.00
+dinner1 = 18.00
+dinner2 = 19.00
+
+if breakfast1 <= convertedtime <= beakfast2 :
+        print("breakfast")
+elif lunch1 <= convertedtime <= lunch2 :
+        print("lunch")
+elif dinner1 <= convertedtime <= dinner2:
+        print("dinner")
 
