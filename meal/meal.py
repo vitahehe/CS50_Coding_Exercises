0d8e87a2dd35a1dc2bfd1bae3b21c7fd107bffbe
vitahehe <vita.stefanija@gmail.com>
2024-07-06 18:18:56 +0000

    Sat, Jul 6, 2024, 8:18 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,14 +1,15 @@
 def main():
     time = str(input("whats the time?").strip().lower())
-    def convert(x,y,z):
+    #def convert(x,y,z):
         return x.replace(y,z)
-    
-    converted_time = float(convert(time, ":", "."))
-    if 07.00 <= converted_time <= 08.00:
+
+    #converted_time = float(convert(time, ":", "."))
+    #if 07.00 <= converted_time <= 08.00:
         print("breakfast time")
-    if 12.00 <= converted_time <= 13.00:
+    #if 12.00 <= converted_time <= 13.00:
         print("lunch time")
-    if 18.00 <= converted_time <= 19.00:
+    #if 18.00 <= converted_time <= 19.00:
         print("dinner time")
+
 if __name__ == "__main__":
     main()
