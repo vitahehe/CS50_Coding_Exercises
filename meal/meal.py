76bb55a9e3479c38fbb665f3f734f6d0256d4dd6
vitahehe <vita.stefanija@gmail.com>
2024-07-06 18:17:33 +0000

    Sat, Jul 6, 2024, 8:17 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,6 +2,7 @@ def main():
     time = str(input("whats the time?").strip().lower())
     def convert(x,y,z):
         return x.replace(y,z)
+    
     converted_time = float(convert(time, ":", "."))
     if 07.00 <= converted_time <= 08.00:
         print("breakfast time")
