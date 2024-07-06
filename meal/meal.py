813695c72e0730edb882265b9e42f344ba54efdf
vitahehe <vita.stefanija@gmail.com>
2024-07-06 18:39:14 +0000

    Sat, Jul 6, 2024, 8:39 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -11,6 +11,9 @@ def main():
     #if 18.00 <= converted_time <= 19.00:
        # print("dinner time")
     def convert(time):
+        times= float(time.replace(":", "."))
+        times.replace(".", )
+
 
 if __name__ == "__main__":
     main()
