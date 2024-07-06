9cae987e6f7cabe5dce04b287ca67a5468092bab
vitahehe <vita.stefanija@gmail.com>
2024-07-06 17:41:40 +0000

    Sat, Jul 6, 2024, 7:41 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,7 +1,10 @@
 time = input("what time is it?").lower().strip()
 
+def replacesymbol(x,y,z):
+      return replacesymbol.replace(y,z)
+
 if ":" in time:
-      
+
 def convert(x):
       times = float(x)
       print(times)
