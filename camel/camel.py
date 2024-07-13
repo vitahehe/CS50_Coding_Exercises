aad8d6b3095a8f5206825153de293d08cfefaf52
vitahehe <vita.stefanija@gmail.com>
2024-07-13 18:16:48 +0000

    Sat, Jul 13, 2024, 8:16 PM +02:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,7 +2,7 @@ camelcase = list(input("whats the name of a variable?"))
 
 for letter in camelcase:
     if letter.isupper():
-        print(letter)
-camelcases =
+        separator = letter
+camelcases = str(camelcase)
 
 
