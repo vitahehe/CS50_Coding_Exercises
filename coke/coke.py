bcb3aecb79f5c5d6875eef43cf45c02476aa9f99
vitahehe <vita.stefanija@gmail.com>
2024-07-13 19:43:59 +0000

    Sat, Jul 13, 2024, 9:43 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,10 +1,4 @@
-def main():
-    owed_amount = 50
-    print("Amount Due:", owed_amount)
-    payment = int(input("please pay the correct amount"))
-    
-    while owed_amount != 0 :
-        print("Amount Due:", owed_amount)
+
 
 
 
