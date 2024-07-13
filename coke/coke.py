64a1b86d7b26febc4be63e0002196826f698b626
vitahehe <vita.stefanija@gmail.com>
2024-07-13 20:26:00 +0000

    Sat, Jul 13, 2024, 10:26 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,9 +1,11 @@
 def main():
     payment = int(input("Input Coin: "))
     coke = 50
-    while payment != 0:
-        print("Amount Due: ", coke - payment)
-        payment = int(input("Input Coin: "))
+    if payment == 50 or payment == 25 or payment == 10 or payment == 5:
+        while payment != 0:
+            print("Amount Due: ", coke - payment)
+            payment = int(input("Input Coin: "))
+
 
 
 main()
