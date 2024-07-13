3639f422fac0078f338771230b1d2fa059895d6f
vitahehe <vita.stefanija@gmail.com>
2024-07-13 19:39:27 +0000

    Sat, Jul 13, 2024, 9:39 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,3 +1,13 @@
-print("Amaunt due : 50")
-payment = int(input("please insert the correct amount"))
-if 
+def main():
+    owed_amount = 50
+    print("Amount Due:", owed_amount)
+
+    payment = input("please pay the correct amount")
+    if (payment = 50 or payment = 25 or payment = 5):
+        print()
+
+
+
+
+
+
