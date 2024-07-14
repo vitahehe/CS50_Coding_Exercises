76d5877d3980871d78f136028081a681b16afb52
vitahehe <vita.stefanija@gmail.com>
2024-07-14 14:02:48 +0000

    Sun, Jul 14, 2024, 4:02 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,18 +1,13 @@
-coke = 50
-print("Amount Due: ", coke)
-payment = int(input("Input Coin: "))
-
-if payment == 50 or payment == 25 or payment == 10 or payment ==5 :
-        while coke != 0:
-              print("Amount Due: ", coke - payment)
-              coke = coke - payment
-              if coke != 0:
-                    payment = int(input("Input Coin: "))
-              else:
-                    print("Change Owed: 0")
-
-
-else:
-    print("Amount Due: ", coke)
+def main():
+    payed_amount = 0
+    coke = 50
+    while payed_amount < 50:
+        print("Amount Due: ", coke )
+        payment = int(input("Insert Coin: "))
+        if payment in [50, 25, 10, 5]:
+            payed_amount = payment
+            print("Amount Due: ", coke - payed_amount)
+        else:
+            print("Amount Due: ", coke)
 
 
