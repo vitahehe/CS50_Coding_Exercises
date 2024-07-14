947c63c0ec5563543c400dd32bca1dca11528e67
vitahehe <vita.stefanija@gmail.com>
2024-07-14 13:37:53 +0000

    Sun, Jul 14, 2024, 3:37 PM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,8 +5,8 @@ payment = int(input("Input Coin: "))
 if payment == 50 or payment == 25 or payment == 10 or payment ==5 :
         while payment != 0:
               print("Amount Due: ", coke - payment)
+              payment = coke - payment
               coke = coke - payment
-              payment = 
 
 
 
