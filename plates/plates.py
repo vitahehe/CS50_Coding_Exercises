3880cc5ba60505e13ca8c2d56eb3c7f82f5723c8
vitahehe <vita.stefanija@gmail.com>
2024-07-29 15:15:42 +0000

    Mon, Jul 29, 2024, 5:15 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -14,6 +14,11 @@ def is_valid(s):
             found_digit = True
         elif found_digit:
             return False
+    for i, char in enumerate(s):
+        if char.isdigit():
+            # Check if the first digit is '0'
+            if char == '0':
+                return False
     return s.isalnum() and len(s)> 0 and s[:2].isalpha() and (2 <= len(list(s)) <= 6)
 
 
