0e8d7dc1f6bb1dc809cb47c1365efbc0f63d9a2e
vitahehe <vita.stefanija@gmail.com>
2024-07-29 11:37:11 +0000

    Mon, Jul 29, 2024, 1:37 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,5 +1,6 @@
 def main():
     plate = input("Plate: ")
+    plate_char = list(plate)
     if is_valid(plate):
         print("Valid")
     else:
@@ -7,7 +8,8 @@ def main():
 
 
 def is_valid(s):
-    
+    if 2 <= plate_char <= 6:
+
 
 
 main()
