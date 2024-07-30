a8c71c3e0aa1ca8536dccc0e36b55fdf642de8f4
vitahehe <vita.stefanija@gmail.com>
2024-07-30 12:37:38 +0000

    Tue, Jul 30, 2024, 2:37 PM +02:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,11 +8,15 @@ def main():
 
 
 def is_valid(s):
-    if s.isalnum() and len(s) != 0 and s[:2].is alpha() and (2 <= len(s) <= 6):
+    if s.isalnum() and len(s) != 0 and s[:2].isalpha() and (2 <= len(s) <= 6):
         return True
     for char in s:
-        if char.isdigit():
-            
+        if s[char].isdigit() and s[char + 1].isaplha():
+            return False
+        else:
+            return True
+
+
 
 
 
