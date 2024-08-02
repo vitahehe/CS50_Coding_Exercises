2811f5b19d30191428c256a1d1ea53bf16ac4227
vitahehe <vita.stefanija@gmail.com>
2024-08-02 05:54:16 +0000

    Fri, Aug 2, 2024, 7:54 AM +02:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,11 +1,10 @@
 amount_due = 50
-print("Amount Due :", amount_due)
 
 while amount_due > 0 :
+    print("Amount Due :", amount_due)
     prompt = int(input("Insert Coin: "))
     if prompt in [5, 10, 25]:
             amount_due = amount_due  - prompt
-            print("Amount Due :", amount_due)
     else:
           print("Amount Due: ", amount_due)
 while amount_due < 0:
