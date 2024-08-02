36552fc453c221db5f49c63f3e70f87e76e20048
vitahehe <vita.stefanija@gmail.com>
2024-08-02 05:01:14 +0000

    Fri, Aug 2, 2024, 7:01 AM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,7 +1,10 @@
 def main():
-    Input = input(Something: ).strip().lower()
+    Input = input("Something:" ).strip().lower()
     Changed_Input= shorten(Input)
     print(Changed_Input)
 
     def shorten(s):
-        
+        for char in s:
+            if char == "a" or char =="e" or char == "i" or char =="o"  or char =="u":
+                remove char
+
