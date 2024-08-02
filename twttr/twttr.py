62fce29b03e984333cb4b9b7ba2b5fe2a69605ad
vitahehe <vita.stefanija@gmail.com>
2024-08-02 04:58:44 +0000

    Fri, Aug 2, 2024, 6:58 AM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,2 +1,7 @@
 def main():
-    Input = input(Something: ).
+    Input = input(Something: ).strip().lower()
+    Changed_Input= shorten(Input)
+    print(Changed_Input)
+
+    def shorten(s):
+        
