87aef97850f48dbf884795e7d7611d83ffcce043
vitahehe <vita.stefanija@gmail.com>
2024-08-02 06:32:55 +0000

    Fri, Aug 2, 2024, 8:32 AM +02:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,5 +1,14 @@
 camelCase = input("camelCase: ")
+snake_case_l
 for char in camelCase:
-    if char is char.isupper():
+        if char.isupper():
+            if snake_case_list:
+                snake_case_l.append('_')
+            snake_case_list.append(char.lower())
+        else:
+            # Otherwise, just append the character
+            snake_case_list.append(char)
 
-        print(camelCase.replace(char, "_" + char.lower()))
+    # Join the list into a single string
+    snake_case_string = ''.join(snake_case_list)
+    return snake_case_string
