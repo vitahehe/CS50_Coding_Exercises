829511a8133316b2e0588eec73e102fc587d622b
vitahehe <vita.stefanija@gmail.com>
2024-08-06 15:25:29 +0000

    Tue, Aug 6, 2024, 5:25 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,4 +1,23 @@
-date = input("Date: ")
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
+
+
+date = input("Date: ")
 try:
-    
+    elements = date.split("/")
+    print(f"{elements[2]}-{elements[0]}-{elements[1]}")
+except:
+    elements = date.split
