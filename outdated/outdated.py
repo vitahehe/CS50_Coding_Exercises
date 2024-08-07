2be69ef1522b0b36dc1a9764a273edbe64e05839
vitahehe <vita.stefanija@gmail.com>
2024-08-07 15:55:52 +0000

    Wed, Aug 7, 2024, 5:55 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -23,11 +23,18 @@ def convert_date(s):
         if new_date[0] in months and new_date[1] in days_monts:
             if len(new_date[1]) == 1:
                 new_date[1] = "0" + new_date[1]
+            if len(new_date[0]) == 1:
+                new_date[0] = "0" + new_date[0]
         month_index = months.index(new_date[0])+1
         print(f"{new_date[2]}-{month_index}-{new_date[1]}")
     except ValueError:
         date = s.split("/")
-        if len(date) == 
+        if len(date[0]) == 1:
+            date[0]= "0" + date[0]
+        if len(date[1]) == 1:
+            date[1] = "0" + date[1]
+    
+
 
 
 
