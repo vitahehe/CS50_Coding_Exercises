8c0e3db4bbfde5e1985cb88639743bcc6e7166f4
vitahehe <vita.stefanija@gmail.com>
2024-08-07 15:42:57 +0000

    Wed, Aug 7, 2024, 5:42 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,11 +16,15 @@ months = [
 days_monts = list(range(1,32))
 
 def convert_date(s):
+    datee = input(Date: )
     try:
         date = s.replace(",","")
         new_date = date.split(" ")
         if new_date[0] in months and new_date[1] in days_monts:
+            if len(new_date[1]) == 1:
+                new_date[1] = "0" + new_date[1]
             
+
 
 
 
