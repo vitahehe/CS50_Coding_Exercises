989b69215a19dea91d52d544c36a9f9b8dd3cf2f
vitahehe <vita.stefanija@gmail.com>
2024-08-07 18:35:59 +0000

    Wed, Aug 7, 2024, 8:35 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -16,7 +16,8 @@ months = [
 days_monts = [str(i) for i in range(1,32)]
 
 def convert_date(s):
-    try:
+
+
         date = s.replace(",","")
         new_date = date.split(" ")
         if new_date[0] in months and new_date[1] in days_monts:
@@ -25,13 +26,15 @@ def convert_date(s):
         month_index = months.index(new_date[0])+1
         month_index_str = f"{month_index:02}"
         print(f"{new_date[2]}-{month_index_str}-{new_date[1]}")
-    except ValueError:
-        date = s.split("/")
-        if len(date[0]) == 1:
-            date[0]= "0" + date[0]
-        if len(date[1]) == 1:
-            date[1] = "0" + date[1]
-        print(f"{date[2]}-{date[0]}-{date[1]}")
+
+        else:
+            date = s.split("/")
+            if len(date[0]) == 1:
+                date[0]= "0" + date[0]
+            if len(date[1]) == 1:
+                date[1] = "0" + date[1]
+            print(f"{date[2]}-{date[0]}-{date[1]}")
+
 
 convert_date(input("Date: "))
 
