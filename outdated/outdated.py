32e81bb2c0eedfb33601f624cda6a751b8a0870c
vitahehe <vita.stefanija@gmail.com>
2024-08-07 15:36:43 +0000

    Wed, Aug 7, 2024, 5:36 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,23 +15,8 @@ months = [
 
 days_monts = list(range(1,32))
 
-while True:
-    date = input("Date: ")
-    try:
-        elements = date.split("/")
-        if len(elements[1]) ==1:
-            elements[1] = "0" + elements[1]
-        if  len(elements[0]) ==1:
-            elements[0] ="0" + elements[0]
-        print(f"{elements[2]}-{elements[0]}-{elements[1]} ")
-    except:
-        new_date = date.replace(",", "")
-        elements = new_date.split(" ")
-        if elements[0] in months and elements[1] in days_monts:
-            if len(elements[1]) ==1:
-                elements[1] = "0" + elements[1]
-
-        print(f"{elements[2]}-{months.index(elements[0]) + 1}-{elements[1]} ")
+def convert_date(s):
+    
 
 
 
