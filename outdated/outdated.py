142e3d18127041368aad01f44cee811f9e7d90a4
vitahehe <vita.stefanija@gmail.com>
2024-08-07 15:28:03 +0000

    Wed, Aug 7, 2024, 5:28 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,12 +18,12 @@ months = [
 date = input("Date: ")
 while True:
     try:
-    elements = date.split("/")
-    if len(elements[1]) ==1:
-        elements[1] = "0" + elements[1]
-    if  len(elements[0]) ==1:
-        elements[0] ="0" + elements[0]
-    print(f"{elements[2]}-{elements[0]}-{elements[1]} ")
+        elements = date.split("/")
+        if len(elements[1]) ==1:
+            elements[1] = "0" + elements[1]
+        if  len(elements[0]) ==1:
+            elements[0] ="0" + elements[0]
+        print(f"{elements[2]}-{elements[0]}-{elements[1]} ")
     except:
         new_date = date.replace(",", "")
         elements = new_date.split(" ")
