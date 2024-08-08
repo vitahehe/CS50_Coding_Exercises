14b37fa72358f486fb618622aabce46e794f97f9
vitahehe <vita.stefanija@gmail.com>
2024-08-08 12:45:08 +0000

    Thu, Aug 8, 2024, 2:45 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,9 @@
-def main():
-    x= get_percantage(input("Fraction:"))
 
-def get_percantage(s):
+
+def get_percantage():
     while True:
         try:
+            s = input("Fraction: ")
             fraction_list = s.split("/")
             fraction_list = [int(i) for i in fraction_list]
             result = (fraction_list[0] / fraction_list[1]) * 100
@@ -23,7 +23,5 @@ def get_percantage(s):
         except ZeroDivisionError:
             input("Fraction:")
 
-
-main()
 
 
