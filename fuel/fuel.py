05d113ec296469d5e50019a92b969d049e034e0c
vitahehe <vita.stefanija@gmail.com>
2024-08-08 12:27:19 +0000

    Thu, Aug 8, 2024, 2:27 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,12 @@
 def main():
 
-    x = get_percantage(input("Fraction: "))
 
 def get_percantage(s):
     while True:
-        fraction_list = s.split("/")
+        try:
+            s = input("Fraction:")
+            fraction_list = s.split("/")
+            fraction_list[0]
+
 
 
