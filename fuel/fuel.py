e7d8fb410fcd7b2f032dcf22d6948aefa5c27d38
vitahehe <vita.stefanija@gmail.com>
2024-08-08 13:03:04 +0000

    Thu, Aug 8, 2024, 3:03 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,10 +1,1 @@
 
-x = input("Fraction: ")
-try:
-    fraction_list = x.split("/")
-    fraction_list = [int(i) for i in fraction_list]
-    result = (fraction_list[0] / fraction_list[1]) * 100
-    print( result ,"%")
-    break
-
-
