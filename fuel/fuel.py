6d6f5e721113e3c1271e01cdfba91a2d4b8cf03c
vitahehe <vita.stefanija@gmail.com>
2024-08-08 12:34:13 +0000

    Thu, Aug 8, 2024, 2:34 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,12 +1,18 @@
 def main():
-
+    x= get_percantage(input("Fraction:"))
 
 def get_percantage(s):
     while True:
         try:
-            s = input("Fraction:")
             fraction_list = s.split("/")
-            fraction_list[0]
+            fraction_list = [int(i) for i in fraction_list]
+            result = (fraction_list[0] / fraction_list[1]) * 100
+            print(result,"%")
+        except ValueError:
+            input("Fraction:")
+        else:
+            break
 
+main()
 
 
