f453223f371f90a2f052754827607197aaa3166a
vitahehe <vita.stefanija@gmail.com>
2024-08-10 18:07:15 +0000

    Sat, Aug 10, 2024, 8:07 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,12 +5,12 @@ while True:
         fraction_list = x.split("/")
         fraction_list_int = [int(i) for i in fraction_list]
         result = (fraction_list_int[0] / fraction_list_int[1])* 100
-        if result == 100:
+        if result == 100 or result == 99:
             print("F")
             break
         elif result > 100:
             continue
-        elif result == 0:
+        elif result == 0 or result == 1:
             print("E")
             break
         elif result < 0:
@@ -18,8 +18,10 @@ while True:
         else:
             print(f'{int(result)}%)
             break
+
     except ZeroDivisionError:
         continue
+                  
     except ValueError:
         continue
 
