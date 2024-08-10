689c690e56b3d77078ad9c2eafa305387b5ca1df
vitahehe <vita.stefanija@gmail.com>
2024-08-10 17:57:52 +0000

    Sat, Aug 10, 2024, 7:57 PM +02:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,1 +1,24 @@
-
+while True:
+    try:
+        x = input("Fraction: ")
+        fraction_list = x.split("/")
+        fraction_list_int = [int(i) for i in fraction_list]
+        result = (fraction_list_int[0] / fraction_list_int[1])* 100
+        if result == 100:
+            print("F")
+            break
+        elif result > 100:
+            continue
+        elif result == 0:
+            print("E")
+            break
+        elif result < 0:
+            continue
+        else:
+            print(result,"%")
+            break
+    except ZeroDivisionError:
+        continue
+    except ValueError:
+        continue
+            
