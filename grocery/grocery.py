c6b83647c5eec388d47f4d043c575a8530a16b2c
vitahehe <vita.stefanija@gmail.com>
2024-08-11 15:05:30 +0000

    Sun, Aug 11, 2024, 5:05 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,15 +5,20 @@ def main():
             item = input("Item: ")
             list.append[item]
         except KeyboardInterrupt:
+            make_grolist(list)
 
 
 
 def make_grolist(s):
     dictionary ={}
-    capitalized_list = [i.upper() for str(i) in s]
-    for x in list:
-        if 
+    capitalized_list = [i.upper() for )in s]
+    for x in capitalized_list:
+        if x in dictionary:
+            dictionary[x] += 1
+        else:
+            dictionary[x] = 1
+    print(f"{dictionary.keys()} {dictionary.values()}")
 
-
+main()
 
 
