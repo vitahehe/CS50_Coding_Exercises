8f3342da325279f834d8f54ef509ac72f49cddbe
vitahehe <vita.stefanija@gmail.com>
2024-08-12 14:18:01 +0000

    Mon, Aug 12, 2024, 4:18 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -20,7 +20,14 @@ date= input('Date: ')
 while True:
     try:
         date_list = date.split('/')
-        if date_list[1] in day_monts and date_list[0] in months.index
+        if date_list[1] in day_monts and date_list[0] in str(range(1,13)):
+            if date_list[0] == len(1):
+                date_list[0] = '0' + date_list[0]
+            if date_list[1] == len(1):
+                date_list[1] = '0' + date_list[1]
+            print(f'{date_list[2]}-{date_list[0]}-{date_list[1]}')
+        else:
+            continue
 
 
 
