065e588af554a0596c61ec60793a666b4f39463c
vitahehe <vita.stefanija@gmail.com>
2024-08-12 11:07:14 +0000

    Mon, Aug 12, 2024, 1:07 PM +02:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,17 +5,17 @@ while True:
         item = input("Item: ")
         list.append(item)
 
+    except KeyboardInterrupt:
+        dictionary ={}
+        capitalized_list = [i.upper() for i in ]
+        for x in capitalized_list:
+            if x in dictionary:
+                dictionary[x] += 1
+            else:
+                dictionary[x] = 1
+
+        for keys, values in dictionary.items():
+            print(f'{values} {keys}')
         break
-
-
-    dictionary ={}
-    capitalized_list = [i.upper() for i in s]
-    for x in capitalized_list:
-        if x in dictionary:
-            dictionary[x] += 1
-        else:
-            dictionary[x] = 1
-    for keys, values in dictionary.items():
-        print(f"{values} {keys})
 
 
