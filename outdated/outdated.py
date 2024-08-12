e85f2ef1d3a0523109544ff87ed26b698b6c8287
vitahehe <vita.stefanija@gmail.com>
2024-08-12 14:13:55 +0000

    Mon, Aug 12, 2024, 4:13 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,6 +17,11 @@ days_monts = [str(i) for i in range(1,32)]
 
 date= input('Date: ')
 
+while True:
+    try:
+        date_list = date.split('/')
+        if date_list[1] in day_monts and date_list[0] in months.index
+
 
 
 
