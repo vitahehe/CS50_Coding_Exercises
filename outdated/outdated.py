b829c2e544930d1000f35a0a3c507242434095ce
vitahehe <vita.stefanija@gmail.com>
2024-08-12 14:34:27 +0000

    Mon, Aug 12, 2024, 4:34 PM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,13 +15,14 @@ months = [
 
 days_monts = [str(i) for i in range(1,32)]
 
+dict = {str(i+1): months[i] for i in range(len(months))}
+
 date= input('Date: ')
 
 while True:
-    enumerate(months, start = 1)
     try:
         date_list = date.split('/')
-        if date_list[1] in day_monts and date_list[0] in str(range(1,13)):
+        if date_list[1] in day_monts and date_list[0] in dict.keys():
             if date_list[0] == len(1):
                 date_list[0] = '0' + date_list[0]
             if date_list[1] == len(1):
