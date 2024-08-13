a7f1590d3aa04ba41e572b459368219c6e96d2a5
vitahehe <vita.stefanija@gmail.com>
2024-08-13 08:42:37 +0000

    Tue, Aug 13, 2024, 10:42 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -30,18 +30,22 @@ while True:
                 date_list[1] = '0' + date_list[1]
             print(f'{date_list[2]}-{date_list[0]}-{date_list[1]}')
             break
+        else:
+            continue
 
     except ValueError:
         date_2 = date.replace(',', '')
         date_22 = date_2.split(' ')
         if date_22[0] in months and date_22[1] in day_monts:
             month_index = months.index(date_22[0])+1
-            if len(dict[date_22[0]]) == 1:
+            if len(month_index) == 1:
                 date_22[0] = '0' + date_22[0]
             if len(date_22[1]) == 1:
                 date_2[1] = '0' + date_2[1]
-        print(f'{date_2[2]}-{date_2[0]}')
-        break
+            print(f'{date_2[2]}-{date_2[0]}')
+            break
+        else:
+            continue
 
 
 
