e6ddb0afca4949fa4afac3f4654b97613f764109
vitahehe <vita.stefanija@gmail.com>
2024-08-13 09:04:42 +0000

    Tue, Aug 13, 2024, 11:04 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,39 +15,8 @@ months = [
 
 days_monts = [str(i) for i in range(1,32)]
 
-dict = {str(i+1): months[i] for i in range(len(months))}
 
 
-
-while True:
-    date = input('Date: ')
-    try:
-        date_list = date.split('/')
-        if len(date_list) == 3 and date_list[0].isdigit() and date_list[1].isdigit() and date_list[2].isdigit():
-            if len(str(date_list[0])) == 1:
-                date_list[0] = '0' + date_list[0]
-            if len(str(date_list[1])) == 1:
-                date_list[1] = '0' + date_list[1]
-            print(f'{date_list[2]}-{date_list[0]}-{date_list[1]}')
-            break
-        else:
-            continue
-
-    except ValueError:
-        date_2 = date.replace(',', '')
-        date_22 = date_2.split(' ')
-        if date_22[0] in months and date_22[1] in day_monts:
-            month_index = months.index(date_22[0])+1
-            if month_index < 10:
-                month_str = '0' + str(month_index)
-            else:
-                month_str = str(month_index)
-            if len(date_22[1]) == 1:
-                date_2[1] = '0' + date_2[1]
-            print(f'{date_2[2]}-{date_2[0]}')
-            break
-    else:
-        continue
 
 
 
