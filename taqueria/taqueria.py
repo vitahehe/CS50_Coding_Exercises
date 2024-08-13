2d809407d94dee7272ff15112f800f9b4d414d82
vitahehe <vita.stefanija@gmail.com>
2024-08-13 09:08:11 +0000

    Tue, Aug 13, 2024, 11:08 AM +02:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -2,7 +2,7 @@ menu = {
     "Baja Taco": 4.25,
     "Burrito": 7.50,
     "Bowl": 8.50,
-    "Nachos": 11.00,c
+    "Nachos": 11.00,
     "Quesadilla": 8.50,
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
