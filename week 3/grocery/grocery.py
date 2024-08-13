8ca2b4348749cf21e5a03d9805e7af00f04238fe
vitahehe <vita.stefanija@gmail.com>
2024-08-13 09:51:29 +0000

    Tue, Aug 13, 2024, 11:51 AM +02:00

--- week 3/grocery/grocery.py
+++ week 3/grocery/grocery.py
@@ -1,25 +1,27 @@
+items = []
 
-list = []
 while True:
     try:
         item = input(" ")
-        list.append(item)
+        items.append(item)
 
     except KeyboardInterrupt:
-        dictionary ={}
-        capitalized_list = [i.upper() for i in list]
+        dictionary = {}
+
+        capitalized_list = [i.upper() for i in items]
         for x in capitalized_list:
             if x in dictionary:
                 dictionary[x] += 1
             else:
                 dictionary[x] = 1
+
         numbers = list(dictionary.values())
-        fruts = list(dictionary.keys())
-        for item1, item2 in zip(numbers, fruts):
+        fruits = list(dictionary.keys())
+
+        for item1, item2 in zip(numbers, fruits):
             print(f"{item1:<10} {item2}")
         break
 
     except EOFError:
         exit(0)
-
 
