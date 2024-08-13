add235edaa89d055354105f47a584e04e3f3d2c6
vitahehe <vita.stefanija@gmail.com>
2024-08-13 08:50:52 +0000

    Tue, Aug 13, 2024, 10:50 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -38,8 +38,10 @@ while True:
         date_22 = date_2.split(' ')
         if date_22[0] in months and date_22[1] in day_monts:
             month_index = months.index(date_22[0])+1
-            if len(month_index) == 1:
-                date_22[0] = '0' + date_22[0]
+            if month_index < 10:
+                month_str = '0' + str(month_index)
+            else:
+                month_str = str(month_index)
             if len(date_22[1]) == 1:
                 date_2[1] = '0' + date_2[1]
             print(f'{date_2[2]}-{date_2[0]}')
