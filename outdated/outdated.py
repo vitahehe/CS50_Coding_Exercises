cec7df435666ca28ef6ca86fd0699cc474dd02ea
vitahehe <vita.stefanija@gmail.com>
2024-08-13 08:10:14 +0000

    Tue, Aug 13, 2024, 10:10 AM +02:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,9 +17,10 @@ days_monts = [str(i) for i in range(1,32)]
 
 dict = {str(i+1): months[i] for i in range(len(months))}
 
-date= input('Date: ')
+
 
 while True:
+    date = input('Date: ')
     try:
         date_list = date.split('/')
         if date_list[1] in day_monts and date_list[0] in dict.keys():
@@ -29,7 +30,12 @@ while True:
                 date_list[1] = '0' + date_list[1]
             print(f'{date_list[2]}-{date_list[0]}-{date_list[1]}')
     except ValueError:
-        date_2 = date.split(' ').remove(',')
+        date_2 = date.split(' ')
+        date_22=[]
+        for string in date_2:
+            new_string = string.remove(',')
+            date_22.append(new_string)
+
         if date_2[0] in months and date_2[1] in day_monts:
             if date_2[0] == len(1):
                 date_2[0] = '0' + date_2[0]
