75addcea575f170c9fff8ed994d6a6b3fff0f995
vitahehe <vita.stefanija@gmail.com>
2024-08-14 14:33:14 +0000

    Wed, Aug 14, 2024, 4:33 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,6 +3,8 @@ import sys
 
 try:
     if len(sys.argv) == 0 or len(sys.argv) == 2:
+        if len(sys.argv) == 0:
+            re pyfiglet.figlet_format(sys.a, font = "3-d" )
 
 
 except ValueError:
