e73913841c6269e1ff194ed3d974489f5beeff99
vitahehe <vita.stefanija@gmail.com>
2024-08-14 14:40:15 +0000

    Wed, Aug 14, 2024, 4:40 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,17 +1,13 @@
 import pyfiglet
 import sys
 
-try:
-    imput = input('Input: ')
-    if len(sys.argv) == 0 or len(sys.argv) == 2:
-        if len(sys.argv) == 0:
-            results = pyfiglet.figlet_format(imput, font = 'random')
-            print(results)
-        elif len(sys.argv) == 2:
-            results = pyfiglet.figlet_format(imput , font = sys.argv[2])
-            print(results)
+imput = input('Input: ')
+if len(sys.argv) == 0 or len(sys.argv) == 2:
+    if len(sys.argv) == 0:
+        results = pyfiglet.figlet_format(imput, fon)
+        print(results)
+    elif len(sys.argv) == 2:
+        results = pyfiglet.figlet_format(imput , font = sys.argv[2])
+        print(results)
 
 
-
-except ValueError:
-    sys.exit()
