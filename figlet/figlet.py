6c951e1f5df53a302d6f0c5417364fdf589523bb
vitahehe <vita.stefanija@gmail.com>
2024-08-14 14:41:55 +0000

    Wed, Aug 14, 2024, 4:41 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,11 @@
 import pyfiglet
 import sys
+import random
 
 imput = input('Input: ')
 if len(sys.argv) == 0 or len(sys.argv) == 2:
     if len(sys.argv) == 0:
-        results = pyfiglet.figlet_format(imput, fon)
+        results = pyfiglet.figlet_format(f'imput', font =random.choice())
         print(results)
     elif len(sys.argv) == 2:
         results = pyfiglet.figlet_format(imput , font = sys.argv[2])
