986f1fed7ceaf4f44b22b59fb82c75097bbb3737
vitahehe <vita.stefanija@gmail.com>
2024-08-14 14:47:41 +0000

    Wed, Aug 14, 2024, 4:47 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -7,12 +7,11 @@ imput = input('Input: ')
 available_fonts = pyfiglet.FigletFont.getFonts()
 random_font = random.choice(available_fonts)
 
-if len(sys.argv) == 0 or len(sys.argv) == 2:
-    if len(sys.argv) == 0:
-        results = pyfiglet.figlet_format(f'{imput}', font =random_font)
-        print(results)
-    elif len(sys.argv) == 2:
-        results = pyfiglet.figlet_format(f'{imput}' , font = sys.argv[2])
-        print(results)
+if len(sys.argv) == 1:
+    results = pyfiglet.figlet_format(f'{imput}', font =random_font)
+    print(results)
+elif len(sys.argv) == 3:
+    results = pyfiglet.figlet_format(f'{imput}' , font = sys.argv[2])
+    print(results)
 
 
