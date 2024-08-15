2a4f0923f200dbc45e36c747521ff3a2b8f008e6
vitahehe <vita.stefanija@gmail.com>
2024-08-15 09:04:01 +0000

    Thu, Aug 15, 2024, 11:04 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,10 +1,13 @@
 
-
+names = []
 while True:
     try:
         name = input('Name: ')
-        
+        names.append(name)
+
     except KeyboardInterrupt:
+        for x in names:
+            
     except EOFError:
         exit(o)
 
