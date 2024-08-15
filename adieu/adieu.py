6e545a4c64e6a7859457f7530c4609fa92fe0c24
vitahehe <vita.stefanija@gmail.com>
2024-08-15 19:44:19 +0000

    Thu, Aug 15, 2024, 9:44 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,4 +1,4 @@
-impport sys
+import sys
 import inflect
 
 names = []
@@ -7,7 +7,11 @@ while True:
         name = input('Name: ')
         names.append(name)
     except KeyboardInterrupt:
-        
+        for i in range(1,(len(names)*2) -1 , 2):
+            names.insert(i, ',')
+        names[-2] = 'and'
+        for item in names:
+            print(item)
         sys.exit()
 
 
