1718bf42fcd76444606d7f280d7d3be802242a4f
vitahehe <vita.stefanija@gmail.com>
2024-08-16 12:53:14 +0000

    Fri, Aug 16, 2024, 2:53 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,12 +9,14 @@ while True:
         if len(names) == 0:
             sys.exit()
         elif len(names) == 2:
-            final_names = names[0] + 'and' + names[1]
+            final_names = names[0] + ' and ' + names[1]
         elif len(names) ==1:
             final_names = names[0]
         else:
             final_names = ', '.join(names[:-1]) + ', and ' + names[-1]
-        sys.exit(f'Adieu, Adieu, to {final_names}')
+        print(f'Adieu, Adieu, to {final_names}')
+        sys.exit(0)
+
 
 
     except EOFError:
