bf224aa924b851f2925f9ac306e1d20be1f675d6
vitahehe <vita.stefanija@gmail.com>
2024-08-16 12:42:27 +0000

    Fri, Aug 16, 2024, 2:42 PM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -8,11 +8,12 @@ while True:
     except KeyboardInterrupt:
         if len(names) == 0:
             sys.exit()
-        e
+        elif len(names) == 2:
+            final_names = names[0] + 'and' + names[1]
         elif len(names) ==1:
             final_names = names[0]
         else:
-            final_names = ','.join(names[:-1]) + ', and ' + names[-1]
+            final_names = ', '.join(names[:-1]) + ', and ' + names[-1]
         print(f'Adieu, Adieu, to {final_names}')
         sys.exit()
 
