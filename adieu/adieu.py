5814f604e2e3f21c1026c6b85ca0d689a3adfb28
vitahehe <vita.stefanija@gmail.com>
2024-08-16 07:50:31 +0000

    Fri, Aug 16, 2024, 9:50 AM +02:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -9,7 +9,7 @@ while True:
         if len(names) == 0:
             sys.exit()
         new_names=[]
-        for i in range(len(names[:-1])):
+        for i in range(names[:-1]):
             new_element = i + ','
             new_names.append(new_element)
         new_names[-1] = names[-1]
