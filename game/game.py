b44f7ecfcf345dca3e55a1a2eadbc4eea5874686
vitahehe <vita.stefanija@gmail.com>
2024-08-17 12:39:40 +0000

    Sat, Aug 17, 2024, 2:39 PM +02:00

--- game/game.py
+++ game/game.py
@@ -3,6 +3,11 @@ import random
 intiger = int(input('Level: '))
 while intiger <= 0:
     intiger = int(input('Level: '))
-random_intiger = 
+random_intiger = random.randrange(1, intiger)
+
+while True:
+    gess = int(input('Guess: '))
+    if gess 
+
 
 
