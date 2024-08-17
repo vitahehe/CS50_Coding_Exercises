6ae9003395b95fa809b1dc809957e1e851bd39f7
vitahehe <vita.stefanija@gmail.com>
2024-08-17 13:04:29 +0000

    Sat, Aug 17, 2024, 3:04 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,15 +2,19 @@ import random
 
 
 def main():
-    ...
+
 
 
 def get_level():
-    ...
+    while level < 0 or level > 3 :
+        level = int(input('Level: '))
+
+
+
 
 
 def generate_integer(level):
-    ...
+
 
 
 if __name__ == "__main__":
