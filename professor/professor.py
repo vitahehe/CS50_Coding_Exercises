168797462ebc826b6a2e0c0bfea61969d907b498
vitahehe <vita.stefanija@gmail.com>
2024-08-18 11:45:14 +0000

    Sun, Aug 18, 2024, 1:45 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -10,7 +10,11 @@ def main():
         solutions.append(answer)
         user_q= int(input(f'{question} = '))
         print(user_q)
-        if user_q == solutions[]
+        for i in range(3):
+            if user_q == answer:
+                break
+            else:
+                print('EEE')
 
 
 
