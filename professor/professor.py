9cb695e8ccd61b4a90b69d920b9ab72dcae133de
vitahehe <vita.stefanija@gmail.com>
2024-08-19 18:05:34 +0000

    Mon, Aug 19, 2024, 8:05 PM +02:00

--- professor/professor.py
+++ professor/professor.py
@@ -33,13 +33,14 @@ def main():
 
 
 def get_level():
-    while True:
+    try:
+       while True:
         level = int(input('Level: '))
         if level == 1 or level ==2 or level ==3:
             return level
         else:
             continue
-
+    except ValueError
 
 
 def generate_integer(level):
