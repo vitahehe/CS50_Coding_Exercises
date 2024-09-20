091348cbc2e92f70038a90faba7fa767f3232bc3
vitahehe <vita.stefanija@gmail.com>
2024-09-20 14:13:16 +0000

    Fri, Sep 20, 2024, 4:13 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,10 +1,12 @@
 import sys
 import requests
-
 try:
-    if len(sys.argv) == 1:
+    try:
+        if len(sys.argv) == 1:
         sys.exit('Missing command-line argument ')
     bit= float(sys.argv[1])
-except ValueError:
-    sys.exit('Command-line argument is not a number')
+    except ValueError:
+        sys.exit('Command-line argument is not a number')
+    
+
 
