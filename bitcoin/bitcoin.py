6ead8c64a7a2cafaf9b2e60a1aead7504d469abc
vitahehe <vita.stefanija@gmail.com>
2024-09-20 14:04:02 +0000

    Fri, Sep 20, 2024, 4:04 PM +02:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,8 +3,7 @@ import sys
 try:
     if len(sys.argv) == 1:
         sys.exit('Missing command-line argument ')
-    float(sys.argv[1] == float:
-        bitcoin = sys.argv[1]
+    bit= float(sys.argv[1])
 except ValueError:
     sys.exit('Command-line argument is not a number')
 
