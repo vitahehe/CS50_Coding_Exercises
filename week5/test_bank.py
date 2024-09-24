5b33c355ec6f45af7a8962de3211d219713c1a34
vitahehe <vita.stefanija@gmail.com>
2024-09-24 12:18:21 +0000

    Tue, Sep 24, 2024, 2:18 PM +02:00

--- week5/test_bank.py
+++ week5/test_bank.py
@@ -2,11 +2,9 @@ from bank import value
 
 def test_first():
     assert value('hello')== 0
-    assert value('HELLO')== 0
 
 def test_second():
     assert value('hey')== 20
-    assert value('HEY')== 20
 
 def test_third():
     assert value('bonjours') == 100
