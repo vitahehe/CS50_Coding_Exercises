0cb66a65ad0e724916b74f50f122f689ff63ab8d
vitahehe <vita.stefanija@gmail.com>
2024-09-24 10:37:31 +0000

    Tue, Sep 24, 2024, 12:37 PM +02:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,13 +1,3 @@
-def shorten(s):
-
-        vowels ="aeiouAEIOU"
-        result = []
-        for char in s:
-            if char not in vowels:
-                result.append(char)
-        return ''.join(result)
-
-
 def main():
     Input = input("Something:" )
     Changed_Input= shorten(Input)
