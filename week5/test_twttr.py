c74281acf261af3b7feae76e92eb7f24c8e0ce89
vitahehe <vita.stefanija@gmail.com>
2024-09-24 10:50:16 +0000

    Tue, Sep 24, 2024, 12:50 PM +02:00

--- week5/test_twttr.py
+++ week5/test_twttr.py
@@ -2,9 +2,9 @@ from twttr import shorten
 
 
 def test_aeiou_remove():
-    assert 'hello' == 'hll'
-    assert 'lole'=='ll'
+    assert shorten('hello') == 'hll'
+    assert shorten('lole')=='ll'
 def test_AEIOU_remove():
-    assert 'HELLO' == 'HLL'
-    assert 'POLEO' =='PL'
+    assert shorten('HELLO') == 'HLL'
+    assert shorten('POLEO') =='PL'
 
