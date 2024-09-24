df3c55dbb2c27818d3f901cf6e886916bb76fff2
vitahehe <vita.stefanija@gmail.com>
2024-09-24 10:55:51 +0000

    Tue, Sep 24, 2024, 12:55 PM +02:00

--- week5/test_twttr.py
+++ week5/test_twttr.py
@@ -8,5 +8,6 @@ def test_AEIOU_remove():
     assert shorten('HELLO') == 'HLL'
     assert shorten('POLEO') =='PL'
 def test_numbers():
-    if exit(0):
-        assert exit(1)
+    assert shorten(hello1) == 'hll1'
+def test_punct():
+    assert shorten(hello!!!)='hll!!!'
