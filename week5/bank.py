691c73d95f6c524491523708ddaebc16abef4a1f
vitahehe <vita.stefanija@gmail.com>
2024-09-24 11:11:34 +0000

    Tue, Sep 24, 2024, 1:11 PM +02:00

--- week5/bank.py
+++ week5/bank.py
@@ -1,10 +1,15 @@
 def main():
     juging = value(input("Greeting good sir").strip().lower())
+    pri
 
 def value(greeting):
      if 'hello' in greeting:
          x=0
-     elif 'h'==
+     elif 'h'== greeting[0]:
+         x=20
+     else:
+         x=100
+     return x
 
 
 
