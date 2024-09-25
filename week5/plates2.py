87010b64a2e7987811e31467b0b599bd752cad61
vitahehe <vita.stefanija@gmail.com>
2024-09-25 19:47:31 +0000

    Wed, Sep 25, 2024, 9:47 PM +02:00

---
+++ week5/plates2.py
@@ -0,0 +1,27 @@
+def main():
+
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if not  (s.isalnum() and len(s) != 0 and s[:2].isalpha() and (2 <= len(s) <= 6)):
+        return False
+    for i in range(len(s) - 1):
+        if s[i].isdigit() and s[i + 1].isalpha():
+            return False
+    for char in s:
+        if char.isdigit():
+            if char == "0":
+                return False
+            break
+    return True
+
+
+
+
+
+main()
